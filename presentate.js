4f14664d98b02e3b415c61c8a31827626a794ff8
Bryan English <bryan@bryanenglish.com>
2014-06-21 02:33:41 -0700

    new features, new help, new separated code
    * `presentate -h` will now give a list of commands.
    * Can now specify top and left padding.
    * Can output all the slides with `presentate -A`
    * Telnet moved into its own module.
    * Fixed the example slides file.

--- presentate.js
+++ presentate.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
 var colorsTmpl = require('colors-tmpl'),
-    keypress = require('keypress'),
-    net = require('net'),
-    stream = require('stream');
+    keypress = require('keypress');
+
+var telnet = require('./lib/telnet')(presentate);
 
 function progressive(arr) {
   for (var i = 0; i < arr.length; i++) {
     if (typeof arr[i] === 'function') arr[i] = arr[i]();
     if (i === 0) continue;
-    arr[i] = arr[i-1] + arr[i];
+    arr[i] = arr[i-1] + '\n' + arr[i];
   }
 }
 
@@ -24,39 +24,17 @@ function flatten(a) {
   return r;
 }
 
+function pad(slide, top, left) {
+  return Array(top+1).join('\n') + Array(left+1).join(' ') +
+    slide.replace(/\n/g, '\n'+Array(left+1).join(' '));
+}
+
 function colorify(string){
   var newString = string.replace(/\{raw\}(.*?)\{\/raw\}/g, function(m, r) {
     return '{raw}'+r.replace(/\{/g, "OPENBRACKET")+'{/raw}';
   });
   newString = colorsTmpl(newString);
   return newString.replace(/\{\/?raw\}/g,'').replace(/OPENBRACKET/g, '{');
-}
-
-function telnet(slides, port){ 
-  var display = new stream.PassThrough();
-  display.pipe(process.stdout);
-
-  var clients = [];
-  var slides = require('./pslides');
-
-  net.createServer(function(socket){
-    socket.on('error', function(err){
-      if (err.code === 'ECONNRESET') return; // Dont' care about this.
-      throw err; // If it isn't this, it's a big deal.
-    });
-    socket.write("[2J[1;1H");
-    socket.write(presentate.slides[presentate.currentSlide]);
-    clients.push(socket);
-
-    display.pipe(socket);
-
-    socket.on('end', function () {
-      display.unpipe(socket);
-      clients.splice(clients.indexOf(socket), 1);
-    });
-  }).listen(port);
-
-  presentate(slides, process.stdin, display, process.exit);
 }
 
 function quit(write, cb) {
@@ -69,36 +47,39 @@ function quit(write, cb) {
   }
 }
 
-function presentate(slides, stdin, stdout, cb){
+function prepareSlides(slides, top, left) {
+    // deal with progressive slides
+  for (var i = 0; i < slides.length; i++) {
+    if (Array.isArray(slides[i])) progressive(slides[i]);
+    if (typeof slides[i] === 'function') slides[i] = slides[i]();
+  }
+  
+  return flatten(slides).map(colorify).map(function(x){
+    return pad(x, top, left);
+  });
+}
+
+function presentate(slides, top, left, stdin, stdout, cb){
   cb = typeof cb === 'function' ? cb : null;
 
   var write = function(x){stdout.write(x)};
 
   keypress(stdin);
-  
+
   function showSlide(n, prev){
     presentate.currentSlide = n;
     var thisSlide = presentate.slides[n];
     var prevSlide = presentate.slides[prev];
-    if (thisSlide.indexOf(prevSlide) !== 0) {
-      write("[2J[1;1H"); // clear screen and return to top left
-      write(thisSlide);
-    } else {
-      write(thisSlide.replace(prevSlide, ''));
-    }
+    write("[2J[1;1H"); // clear screen and return to top left
+    write(thisSlide);
     write('\n');
   }
 
-  // deal with progressive slides
-  for (var i = 0; i < slides.length; i++) {
-    if (Array.isArray(slides[i])) progressive(slides[i]);
-    if (typeof slides[i] === 'function') slides[i] = slides[i]();
-  }
-
   // flatten and color
-  presentate.slides = flatten(slides).map(colorify);
+  presentate.slides = prepareSlides(slides, top, left);
 
   stdin.setRawMode(true);
+  //console.log(presentate.slides.join('\n---------------\n'));
   write("[?25l"); //hide cursor
   showSlide(0); // show the first slide
 
@@ -126,20 +107,37 @@ function presentate(slides, stdin, stdout, cb){
   });
 };
 
+presentate.prepareSlides = prepareSlides;
+presentate.telnet = telnet;
 module.exports = presentate;
 
 if (require.main === module) {
-  var port, slideFileName = require('path').resolve(process.cwd(), 'pslides.js');
-  for (var i = 2; i < process.argv.length; i++) {
-    if (process.argv[i].match(/.*\.js$/))
-      slideFileName = process.argv[i];
-    if (!isNaN(parseInt(process.argv[i])))
-      port = parseInt(process.argv[i]);
+
+  var program = require('commander');
+  program.version(require('./package').version)
+    .usage('[options]')
+    .option('-T --telnet [num]', 'telnet port number (optional)')
+    .option('-t --top [num]', 'lines of padding from the top (default: 1)')
+    .option('-l --left [num]', 'columns of padding from the left (default: 3)')
+    .option('-f --file [filename]', 'a slides.js file (default: ./pslides.js)')
+    .option('-A --all ', 'show all the slides at once, delimited by "\\n---\\n"')
+    .parse(process.argv);
+  var port, slideFileName = './pslides.js';
+  var slides = require(program.file || slideFileName);
+  var top = 1;
+  var left = 3;
+  if (slides.slides) {
+    top = slides.top || top;
+    left = slides.left || left;
+    slides = slides.slides;
   }
-  var slides = require(slideFileName.replace(/\.js$/, ''));
-  if (port) {
-    telnet(slides, port); 
+  top = parseInt(program.top || top);
+  left = parseInt(program.left || left);
+  if (program.all) {
+    console.log(prepareSlides(slides, top, left).join('\n---\n'));
+  } else if (program.telnet) {
+    telnet(slides, top, left, program.telnet); 
   } else {
-    presentate(slides, process.stdin, process.stdout);
+    presentate(slides, top, left, process.stdin, process.stdout);
   }
 }
--- pslides.js
+++ pslides.js
@@ -1,22 +1,20 @@
-var colorsTmpl = require('colors-tmpl');
-
 var demoColors = "red|yellow|green|blue|white|grey|cyan|rainbow|magenta|underline|bold|italic|inverse|zebra".split('|').map(function(c){
   var chunk = "{"+c+"}"+c+"{/"+c+"}";
-  return "\n    "+chunk+"\n\n\n        Source: {raw}"+chunk+"{/raw}";
+  return chunk+"\n\n\n     Source: {raw}"+chunk+"{/raw}";
 });
 
 
 var slides = [
-  '\n    {bold}{underline}Presentate{/underline}{/bold} allows you to make\n    command-line presentations using node.js',
-  '\n    Use left and right arrow keys to navigate.',
+  '{bold}{underline}Presentate{/underline}{/bold} allows you to make\ncommand-line presentations using node.js',
+  'Use left and right arrow keys to navigate.',
   [
-    '\n    You can use {rainbow}colors.js{/rainbow} if you want.',
-    '\n    It\'s included, as {bold}colors-tmpl{/bold}.',
-    '\n\n    For example...'
+    'You can use {rainbow}colors.js{/rainbow} if you want.',
+    'It\'s included, as {bold}colors-tmpl{/bold}.',
+    '\nFor example...'
   ]
 ];
 
 slides = slides.concat(demoColors);
-slides.push("\n    https://github.com/bengl/presentate\n\n    Fork it! Use it!\n\n            -- @bengl");
+slides.push("https://github.com/bengl/presentate\n\nFork it! Use it!\n\n        -- @bengl");
 
 module.exports = slides;
